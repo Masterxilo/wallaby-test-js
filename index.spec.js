9190ad2d0c4e84abc25ec9f1c71cb79f54c052d1
Paul Frischknecht <paul.frischknecht@bit.admin.ch>
2022-02-10 14:26:55 +0100

    init

---
+++ index.spec.js
@@ -0,0 +1,12 @@
+//import { f } from ".";
+// vscode infers module import style:
+//  https://code.visualstudio.com/docs/languages/javascript
+const {f} = require('.');
+const { g } = require('./index2');
+
+describe('Name of the group', () => {
+    it('should ', () => {
+        expect(f()).toBe(40);
+        expect(g()).toBe(42);
+    });
+});
